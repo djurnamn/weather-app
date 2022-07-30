f792d5bfaefcbed8aaf757bb50f7c5b793a13fbc
en människa som heter björn <me+github@bjornpetersson.com>
2022-07-30 23:13:22 +0200

    Set up store with Pinia and vueUse

---
+++ src/store/index.js
@@ -0,0 +1,32 @@
+import { defineStore } from 'pinia'
+import { useStorage } from '@vueuse/core'
+
+export const useMainStore = defineStore({
+    id: 'main',
+    state: () => ({
+        favorites: useStorage('favorites', []),
+    }),
+    getters: {
+        getAllFavorites() {
+            return this.favorites
+        },
+        favoritesInclude: (state) => {
+            return (id) => state.favorites.includes(id)
+        },
+        favoritesEmpty() {
+            return this.favorites.length <= 0
+        }
+    },
+    actions: {
+        addFavorite(id) {
+            this.favorites.push(id);
+        },
+        removeFavorite(id) {
+            const index = this.favorites.findIndex(favorite => id === favorite)
+
+            if (index || index === 0) {
+                this.favorites.splice(index, 1)
+            }
+        }
+    },
+})
