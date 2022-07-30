8bc9d9e1d26a5daa3145e8a83f20fd041942e578
en människa som heter björn <me+github@bjornpetersson.com>
2022-07-30 23:12:47 +0200

    Add Vue router

---
+++ src/router/index.js
@@ -0,0 +1,34 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import WeatherView from '../views/WeatherView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'current',
+    component: WeatherView
+  },
+  {
+    path: '/:id',
+    name: 'day',
+    component: WeatherView
+  },
+  {
+    path: '/favorites',
+    name: 'favorites',
+    component: () => import(/* webpackChunkName: "about" */ '../views/FavoritesView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+  scrollBehavior() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ left: 0, top: 0 })
+      }, 500)
+    })
+  },
+})
+
+export default router
