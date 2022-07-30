d3d20fe6871f4c5a1da4de13eb4fa18df21a3998
en människa som heter björn <me+github@bjornpetersson.com>
2022-07-30 23:14:59 +0200

    Set up favorites view

---
+++ src/views/FavoritesView.vue
@@ -0,0 +1,81 @@
+<template>
+  <div v-if="error">FavoritesView: {{ error }}</div>
+  <div v-else-if="loading">Loading...</div>
+  <div class="FavoritesView" v-else>
+    <header class="FavoritesView__header">
+      <h1 class="FavoritesView__title">Favorites</h1>
+
+      <p class="FavoritesView__description">
+        {{ !isEmpty ? "Here's a list of your your favorite days." : 'No favorites found.' }}
+      </p>
+    </header>
+
+    <section class="FavoritesView__list">
+      <ListOfDays :days="data"  v-if="!isEmpty" />
+    </section>
+  </div>
+</template>
+
+<script>
+import { computed, reactive, ref, toRefs, watch } from "vue"
+import { useMainStore } from "../store/index.js"
+import { getDaysSummary } from "../services/ApiService.js"
+import ListOfDays from "../components/ListOfDays.vue"
+
+export default {
+    async setup() {
+        const store = useMainStore();
+        const loading = ref(null)
+        const error = ref(null)
+        const state = reactive({
+          data: null
+        })
+        const favoriteIds = computed(() => store.getAllFavorites);
+
+        const getViewData = async (ids) => {
+          const response = await getDaysSummary(Object.values(ids))
+
+          return response.data
+        }
+
+        watch(
+          favoriteIds,
+          async (newVal) => {
+            state.data = await getViewData(Object.values(newVal))
+          },
+          { deep: true }
+        )
+
+        try {
+          state.data = await getViewData(Object.values(favoriteIds.value))
+        } catch (e) {
+          error.value = e.response
+        }
+
+        return {
+          loading,
+          error,
+          ...toRefs(state),
+          isEmpty: computed(() => store.favoritesEmpty),
+        }
+    },
+    components: { ListOfDays }
+}
+</script>
+
+<style lang="scss">
+.FavoritesView {
+  &__header,
+  &__list {
+    padding: 1rem 0;
+  }
+
+  &__title {
+    font-size: 1.5rem;
+  }
+
+  &__description {
+    margin-top: 1rem;
+  }
+}
+</style>
