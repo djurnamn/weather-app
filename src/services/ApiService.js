7320a159e3ed7637bdc2d3e4148fe09af0c56bfd
en människa som heter björn <me+github@bjornpetersson.com>
2022-07-31 20:13:30 +0200

    Use Vue accessible env variable for api url in ApiService

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,13 +1,15 @@
 import axios from 'axios'
 
+const apiUrl = process.env.VUE_APP_API_URL || 'http://localhost:3587'
+
 export const getDayFull = async (id = null) => {
     const params = id ? `/${id}` : ''
 
-    return await axios.get(`http://localhost:3587/get-day-full${params}?lat=58.5812&lon=16.158`)
+    return await axios.get(`${apiUrl}/get-day-full${params}?lat=58.5812&lon=16.158`)
 }
 
-export const getDaysSummary = async (ids = false) => {
+export const getDaysSummary = async (ids = null) => {
     const query = ids && Array.isArray(ids) ? `ids=${ids.join(',')}&` : ''
 
-    return await axios.get(`http://localhost:3587/get-days-summary?${query}lat=58.5812&lon=16.158`)
+    return await axios.get(`${apiUrl}/get-days-summary?${query}lat=58.5812&lon=16.158`)
 }
